a1282b3fe1f3d1eb7ba1ed39ca5692d5dd659035
Pablo Martin Garcia <pablomg01@gmail.com>
2023-12-05 15:41:05 +0100

    Completed Lab

---
+++ Lab1.sql
@@ -0,0 +1,18 @@
+-- 1.
+use sakila;
+
+-- 2.
+select * from actor;
+select * from film;
+select * from customer;
+
+-- 3. 
+select title from film;
+
+-- 4.
+select name as language from language;
+
+-- 5.
+select store_id from store;
+select staff_id from staff;
+select first_name from staff;
